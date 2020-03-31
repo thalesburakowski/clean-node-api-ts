c7772a185e2e7f5c1af5139bcda5eb4fde8d7413
Thales Vinícius <thalesburakowski@gmail.com>
2020-03-31 20:17:19 -0300

    feat: ensure EmailValidator returns false if validator returns false

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',

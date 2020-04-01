77c3598ba12b631f185fb62bc98d042784652113
Thales Vinícius <thalesburakowski@gmail.com>
2020-04-01 20:35:50 -0300

    feat: ensure cors is enable

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+
+import app from '../config/app'
+
+describe('CORS Middleware', () => {
+  test('Should enable cors', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_cors')
+      .expect('acess-control-allow-origin', '*')
+      .expect('acess-control-allow-methods', '*')
+      .expect('acess-control-allow-headers', '*')
+  })
+})

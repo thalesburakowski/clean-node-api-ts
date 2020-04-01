94b1949d4960db716b4eda5bbfbcc9fa59d25c6a
Thales Vinícius <thalesburakowski@gmail.com>
2020-04-01 20:31:25 -0300

    feat: ensure requests parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,3 @@
-import express from 'express'
-
-const app = express()
-const PORT = 5050
+import app from './config/app'
+const PORT = 3333
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`))

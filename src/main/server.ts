87ffc742bf72f2bf9207bcea61eb354ca9c4becb
Thales Vinícius <thalesburakowski@gmail.com>
2020-04-02 03:31:44 -0300

    refactor: connect to mongodb before integration tests

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,7 @@
 import app from './config/app'
-const PORT = 3333
-app.listen(PORT, () => console.log(`Server running at port ${PORT}`))
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
+
+MongoHelper.connect(env.MONGO_URL).then(() => {
+  app.listen(env.PORT, () => console.log(`Server running at port ${env.PORT}`))
+}).catch(console.error)
